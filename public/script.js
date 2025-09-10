d428dc55e2c4cf7b6cbfaf49b480ee4d5d817c02
Mohit <200160784+mohit716@users.noreply.github.com>
2025-09-10 05:24:22 -0400

    feat: add theme toggle and apply config-driven dark/light theme

--- public/script.js
+++ public/script.js
@@ -4,5 +4,20 @@ fetch("/config")
     document.getElementById("greeting").textContent=
 	`${config.greeting}, ${config.username}!`;
     document.getElementById("theme").textContent=`Theme is set to: ${config.theme}`;
+    // apply initial theme from config	
 	document.body.className=config.theme;
+    // add toggle button functionality
+	const btn =document.getElementById("toggleBtn");
+	btn.addEventListener("click", ()=>{
+		if(document.body.className==="light"){
+			document.body.className="dark"
+			document.getElementById("theme").textContent="This is set to: dark";
+
+	}
+	
+		else{
+			document.body.className="light";
+			document.getElementById("theme").textContent="Theme is set to: light";
+		}	
+});
 });
