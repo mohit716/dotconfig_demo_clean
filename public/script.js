d68762b6d57fdd38eddacc9547f52f74be1bc1b6
Mohit <200160784+mohit716@users.noreply.github.com>
2025-09-10 02:37:27 -0400

    Applied theme from config (dark/light modes via CSS)

--- public/script.js
+++ public/script.js
@@ -4,4 +4,5 @@ fetch("/config")
     document.getElementById("greeting").textContent=
 	`${config.greeting}, ${config.username}!`;
     document.getElementById("theme").textContent=`Theme is set to: ${config.theme}`;
+	document.body.className=config.theme;
 });
